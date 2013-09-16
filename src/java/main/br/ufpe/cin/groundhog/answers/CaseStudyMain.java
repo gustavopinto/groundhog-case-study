77024060872699078f6d3bd9a03cbd29614c90df
Jesus Jackson Sena da Silva <jesus.jackson.sena@gmail.com>
2013-09-16 13:41:30 -0300

    code to answer issue #49 of groundhog

--- src/java/main/br/ufpe/cin/groundhog/answers/CaseStudyMain.java
+++ src/java/main/br/ufpe/cin/groundhog/answers/CaseStudyMain.java
@@ -3,8 +3,6 @@ package br.ufpe.cin.groundhog.answers;
 public class CaseStudyMain extends Issues {
 
 	public static void main(String[] args) throws Exception {
-		// issue22();
-		// issue47();
-		issue53();
+		issue49();
 	}
 }
